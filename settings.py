8e3c0877a6c10f4fa5ee9a71d4b97ca870e63dd8
wormtooth <ye@wormtooth.com>
2021-03-04 17:32:31 -0500

    restructure folders

--- settings.py
+++ settings.py
@@ -6,8 +6,12 @@ from utils.logger import FileLogger
 # folder
 HERE = os.path.dirname(os.path.realpath(__file__))
 FOLDER = os.path.join(HERE, 'data')
-if not os.path.exists(FOLDER):
-    os.mkdir(FOLDER)
+CLEANED_FOLDER = os.path.join(FOLDER, 'cleaned')
+MODEL_FOLDER = os.path.join(FOLDER, 'model')
+
+for folder in [FOLDER, CLEANED_FOLDER, MODEL_FOLDER]:
+    if not os.path.exists(folder):
+        os.mkdir(folder)
 
 # zhwiki
 ZHWIKI_URL = 'https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2'
@@ -15,13 +19,13 @@ ZHWIKI_PATH = os.path.join(
     FOLDER,
     'zhwiki-latest-pages-articles.xml.bz2'
 )
-ZHWIKI_CLEANED_PATH = os.path.join(FOLDER, 'zhwiki-cleaned.txt')
+ZHWIKI_CLEANED_PATH = os.path.join(CLEANED_FOLDER, 'zhwiki.txt')
 
 # news2016zh
 NEWS2016ZH_FILE_ID = '1TMKu1FpTr6kcjWXWlQHX7YJsMfhhcVKp'
 NEWS2016ZH_ZIP_PATH = os.path.join(FOLDER, 'news2016zh.zip')
 NEWS2016ZH_PATH = os.path.join(FOLDER, 'news2016zh_train.json')
-NEWS2016ZH_CLEANED_PATH = os.path.join(FOLDER, 'news2016zh_cleaned.txt')
+NEWS2016ZH_CLEANED_PATH = os.path.join(CLEANED_FOLDER, 'news2016zh.txt')
 
 
 # stopwords
